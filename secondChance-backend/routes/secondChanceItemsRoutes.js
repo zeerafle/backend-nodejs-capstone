9982a2860008a35fe406d7244ce51ae4ca6714c3
zeerafle <vsefareez@gmail.com>
2025-01-28 03:37:58 -0500

    minor fix

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -46,15 +46,15 @@ router.post('/', upload.single('file'), async(req, res,next) => {
 
         const db = await connectToDatabase();
         const collection = db.collection("secondChanceItems");
-        const secondChanceItem = req.body;
+        let secondChanceItem = req.body;
         const lastItemQuery = await collection.find().sort({'id': -1}).limit(1);
         await lastItemQuery.forEach(item => {
             secondChanceItem.id = (parseInt(item.id) + 1).toString();
         });
-        const date_added = Math.floow(new Date().getTime() / 1000);
+        const date_added = Math.floor(new Date().getTime() / 1000);
         secondChanceItem.date_added = date_added;
         secondChanceItem = await collection.insertOne(secondChanceItem);
-        res.status(201).json(secondChanceItem.ops[0]);
+        res.status(201).json(secondChanceItem);
     } catch (e) {
         next(e);
     }
@@ -64,12 +64,13 @@ router.post('/', upload.single('file'), async(req, res,next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
-        const collection = db.collection('secondChanceItems');
-        const secondChanceItem = collection.findOne({'id': req.params.id});
+        const collection = db.collection("secondChanceItems");
+        const id = req.params.id;
+        const secondChanceItem = await collection.findOne({ id: id });
         if (!secondChanceItem) {
             return res.status(404).send("secondChanceItem not found");
         }
-        res.json(secondChanceItem)
+        res.json(secondChanceItem);
     } catch (e) {
         next(e);
     }
@@ -79,51 +80,46 @@ router.get('/:id', async (req, res, next) => {
 router.put('/:id', async(req, res,next) => {
     try {
         const db = await connectToDatabase();
-        const collection = db.collection('secondChanceItems');
-
+        const collection = db.collection("secondChanceItems");
+        const id = req.params.id;
         const secondChanceItem = await collection.findOne({ id });
         if (!secondChanceItem) {
             logger.error('secondChanceItem not found');
             return res.status(404).json({ error: "secondChanceItem not found" });
         }
-
         secondChanceItem.category = req.body.category;
         secondChanceItem.condition = req.body.condition;
         secondChanceItem.age_days = req.body.age_days;
         secondChanceItem.description = req.body.description;
         secondChanceItem.age_years = Number((secondChanceItem.age_days/365).toFixed(1));
         secondChanceItem.updatedAt = new Date();
-
-        const updatepreloveItem = await collection.findeOneAndUpdate(
+        const updatepreloveItem = await collection.findOneAndUpdate(
             { id },
             { $set: secondChanceItem },
             { returnDocument: 'after' }
         );
-        
-        if (updatepreloveItem) {
-            res.json({"uploaded": "success"});
+        if(updatepreloveItem) {
+            res.json({"uploaded":"success"});
         } else {
-            res.json({"uploaded": "failed"});
+            res.json({"uploaded":"failed"});
         }
     } catch (e) {
         next(e);
     }
 });
-
 // Delete an existing item
 router.delete('/:id', async(req, res,next) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("secondChanceItems");
-
-        const secondChanceItem = await collectionfindOne({ id });
+        const id = req.params.id;
+        const secondChanceItem = await collection.findOne({ id });
         if (!secondChanceItem) {
             logger.error('secondChanceItem not found');
-            return res.status(404).json({ error: 'secondChanceItem not found' });
+            return res.status(404).json({ error: "secondChanceItem not found" });
         }
-        
-        await collection.deleteOne({ id });
-        res.json({"deleted": "success"});
+        const updatepreloveItem = await collection.deleteOne({ id });
+        res.json({"deleted":"success"});
     } catch (e) {
         next(e);
     }
